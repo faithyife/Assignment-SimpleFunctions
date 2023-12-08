8eac5a363609cad45367f377fd6907ab256e7d65
faithyife <faithyuchechi@gmail.com>
2023-12-08 09:56:17 +0100

    created a javasript function

---
+++ function.js
@@ -0,0 +1,7 @@
+//create any function that performs any task. DO NOT USE PROMPT, ALERT OR DOM.
+
+function addNums(num1, num2) {
+  return num1 + num2;
+}
+let result = addNums(3, 5);
+console.log(result);
